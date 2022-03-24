27c0bffeb4a5d21df4d15672d7356e0e0b9cdcb0
Punit Raj <raj.punit3299@gmail.com>
2022-03-24 23:04:33 +0530

    build fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { NewCarsComponent } from './components/home/new-cars/new-cars.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    NewCarsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home/new-cars/new-cars.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewCarsComponent } from './new-cars.component';
+
+describe('NewCarsComponent', () => {
+  let component: NewCarsComponent;
+  let fixture: ComponentFixture<NewCarsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewCarsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewCarsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
